ebb26d44e8795298d58507422c85e66b7c33a361
DarkJoe <gray78181878@gmail.com>
2022-07-28 12:25:00 +0800

    1st c commit

---
+++ test.c
@@ -0,0 +1,27 @@
+// #include <stdio.h>
+
+// int main(){
+
+//   int myNum = 25;
+//   printf("%d", myNum);
+//   return 0;
+// }
+
+
+// #include <stdio.h>
+// int main(){
+
+// int myNum = 5;
+// int myNum1 = 7;
+// printf("My favorite number is: %d%d", myNum,myNum1);
+// }
+
+// #include <stdio.h>
+
+// int main() {
+//   int x = 5;
+//   int y = 6;
+//   int sum = x + y;
+//   printf("%d", sum);
+//   return 0;
+// }
