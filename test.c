4362cc961f7b60dd597b25a80acdc5872da1eeb6
DarkJoe <gray78181878@gmail.com>
2022-07-28 15:40:55 +0800

    3rd commit

--- test.c
+++ test.c
@@ -26,11 +26,84 @@
 //   return 0;
 // }
 
-#include <stdio.h>
 
+// int main() {
+//   char Firstname = 'J';
+//   char lastName = 'a';
+//   printf("%c %c", Firstname, lastName);
+//   return 0;
+// }
+
+// #include <stdio.h>
+
+// int main() {
+//   int x,y,z;
+//   x = 10;
+//   y = 20;
+//   z = 30;
+//   x= ++y+z;
+//   printf("x: %d\n", x);
+//   printf("y: %d\n", y);
+//   printf("z: %d\n",(z++));
+//   return 0;
+// }
+
+
+// Create a string
+// char firstName[30];
+
+// // Ask the user to input some text
+// printf("Enter your first name: \n");
+
+// // Get and save the text
+// scanf("%s", firstName);
+
+// // Output the text
+// printf("Hello %s.", firstName);
+
+// }
+// int x,y;
+// x = 10;
+// y = 20;
+
+// x = y;
+// y = x;
+// printf("x: %d\n" ,x);
+// printf("y: %d" ,y);
+
+// double x = 10, y = 20, temp ;
+
+// temp = x;
+
+// x = y;
+
+// y = temp;
+
+// printf("\nAfter swapping, first number: ", x);
+// printf("\nAfter swapping, wnd number: ", y);
+// return 0;
+// }
+
+
+#include<stdio.h>
 int main() {
-  char Firstname = 'J';
-  char lastName = 'a';
-  printf("%c %c", Firstname, lastName);
+  double first, second, temp;
+  printf("Enter first number: ");
+  scanf("%lf", &first);
+  printf("Enter second number: ");
+  scanf("%lf", &second);
+
+  // value of first is assigned to temp
+  temp = first;
+
+  // value of second is assigned to first
+  first = second;
+
+  // value of temp (initial value of first) is assigned to second
+  second = temp;
+
+  // %.2lf displays number up to 2 decimal points
+  printf("\nAfter swapping, first number = %.2lf\n", first);
+  printf("After swapping, second number = %.2lf", second);
   return 0;
 }
